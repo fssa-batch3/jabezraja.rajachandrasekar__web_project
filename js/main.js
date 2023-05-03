04d43386309b7bc952d93f66c9262804efd29ce2
JABEZRAJA <116268512+JABEZRAJA@users.noreply.github.com>
2023-05-03 14:40:54 +0530

    feat: edit and module added

--- js/main.js
+++ js/main.js
@@ -1,221 +1,230 @@
 // function for logout
-function logout(){
-    alert("Are you sure?")
-    location.href="./pages/login/Login.html"
+function logout() {
+  alert("Are you sure?");
+  window.location.href = "./pages/login/Login.html";
 }
+
 // Increment/decrement counter
-  
-// $(document).ready(function() {
-//    const minus = $('.quantity__minus');
-//    const plus = $('.quantity__plus');
-//    const input = $('.sqb');
-//    minus.click(function(e) {
-//      e.preventDefault();
-//      let value = input.val();
-//      if (value > 1) {
-//        value--;
-//      }
-//      input.val(value);
-//    });
-   
-//    plus.click(function(e) {
-//      e.preventDefault();
-//      let value = input.val();
-//      value++;
-//      input.val(value);
-//    })
-//  });
- 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const minus = document.querySelector(".quantity__minus");
+  const plus = document.querySelector(".quantity__plus");
+  const input = document.querySelector(".sqb");
+
+  minus.addEventListener("click", function (e) {
+    e.preventDefault();
+    let value = parseInt(input.value);
+    if (value > 1) {
+      value--;
+    }
+    input.value = value;
+  });
+
+  plus.addEventListener("click", function (e) {
+    e.preventDefault();
+    let value = parseInt(input.value);
+    value++;
+    input.value = value;
+  });
+});
 
 // JSON FOR DEALS OF THE DAY
 const deals_of_the_day = [
-    {
-       "title":"Best of Electronics",
-       "image":{
-            "source": "assets/images/products/r1.jpg",
-            "alt": "Best of Electronics",
-         },
-       "price-offers":"₹1,777",
-       "sub":"Apple, Samsung  & more",
-       "path":"./pages/products and detial pages/best of electronincs.html",
+  {
+    title: "Best_of_Electronics",
+    image: {
+      source: "assets/images/products/r1.jpg",
+      alt: "Best of Electronics",
     },
-        
-    {
-       "title":"Barware & Bakeware",
-       "image":{
-            "source": "assets/images/products/r2.jpg",
-            "alt": "Barware & Bakeware",
-         },
-       "price-offers":"Upto 67% off",
-       "sub":"Grab & Gone",
-       "path":"",
-    },
+    "price-offers": "₹1,777",
+    sub: "Apple, Samsung  & more",
+  },
 
-    {
-       "title":"Cars & Bikes",
-       "image":{
-            "source": "assets/images/products/r3.jpg",
-            "alt": "Cars & Bikes",
-         },
-       "price-offers":"Top Selling!",
-       "sub":"Starts from ₹299",
-       "path":"",
+  {
+    title: "Barware_Bakeware",
+    image: {
+      source: "assets/images/products/r2.jpg",
+      alt: "Barware & Bakeware",
     },
+    "price-offers": "Upto 67% off",
+    sub: "Grab & Gone",
+  },
 
-    {
-       "title":"Best of Hair Dryers",
-       "image":{
-            "source": "assets/images/products/r4.jpg",
-            "alt": "Best of Hair Dryers",
-         },
-       "price-offers":"Top Brands",
-       "sub":"Starts from ₹499",
-       "path":"",
+  {
+    title: "Cars_Bikes",
+    image: {
+      source: "assets/images/products/r3.jpg",
+      alt: "Cars & Bikes",
     },
+    "price-offers": "Top Selling!",
+    sub: "Starts from ₹299",
+  },
 
-    {
-       "title":"Personal care essential",
-       "image":{
-            "source": "assets/images/products/r6.jpg",
-            "alt": "Personal care essential",
-         },
-       "price-offers":"Min 10% off + extra 15% off",
-       "sub":"Wisper, colgate & nivea",
-       "path":"",
-    }
-]
+  {
+    title: "Best_of_Hair_Dryers",
+    image: {
+      source: "assets/images/products/r4.jpg",
+      alt: "Best of Hair Dryers",
+    },
+    "price-offers": "Top Brands",
+    sub: "Starts from ₹499",
+  },
+
+  {
+    title: "Personal_care_essential",
+    image: {
+      source: "assets/images/products/r6.jpg",
+      alt: "Personal care essential",
+    },
+    "price-offers": "Min 10% off + extra 15% off",
+    sub: "Wisper, colgate & nivea",
+  },
+];
+
 // declare and initialize the variable outside the loop
 let card_1;
 
-for(let i = 0; i<=4; i++){
-
+for (let i = 0; i <= 4; i++) {
   // remove the re-declaration and re-initialization
   card_1 = document.createElement("div");
   card_1.setAttribute("class", "common-col");
 
-  a = document.createElement("a");
-  a.setAttribute("href", deals_of_the_day[i]["path"] );
-  card_1.append(a);
+  //   a = document.createElement("a");
+  //   a.setAttribute("href","./pages/products and detial pages/best of electronincs.html");
+  //   card_1.append(a);
 
-  img_of_electronics = document.createElement("img");
+  const img_of_electronics = document.createElement("img");
   img_of_electronics.setAttribute("class", "product-img");
-  img_of_electronics.setAttribute("src", deals_of_the_day[i]["image"]["source"]);
-  img_of_electronics.setAttribute("alt", deals_of_the_day[i]["image"]["alt"]);
-  a.append(img_of_electronics);
+  img_of_electronics.setAttribute("src", deals_of_the_day[i].image.source);
+  img_of_electronics.setAttribute("alt", deals_of_the_day[i].image.alt);
+  card_1.append(img_of_electronics);
 
-  h3_for_product = document.createElement("h3");
+  // eslint-disable-next-line no-inner-declarations
+  function redirect() {
+    window.location.href = `./pages/products and detial pages/best of electronincs.html?id=${deals_of_the_day[i].title}`;
+  }
+
+  img_of_electronics.addEventListener("click", redirect);
+
+  const h3_for_product = document.createElement("h3");
   h3_for_product.setAttribute("class", "product-title");
-  h3_for_product.innerText=(deals_of_the_day[i]["title"]);
+  h3_for_product.innerText = deals_of_the_day[i].title;
   card_1.append(h3_for_product);
 
-  h4_for_product = document.createElement("h4");
+  const h4_for_product = document.createElement("h4");
   h4_for_product.setAttribute("class", "price-title"); // add the missing prefix
-  h4_for_product.innerText=(deals_of_the_day[i]["price-offers"]);
+  h4_for_product.innerText = deals_of_the_day[i]["price-offers"];
   card_1.append(h4_for_product);
 
-  h6_for_product = document.createElement("h6");
+  const h6_for_product = document.createElement("h6");
   h6_for_product.setAttribute("class", "price");
-  h6_for_product.innerText=(deals_of_the_day[i]["sub"]);
+  h6_for_product.innerText = deals_of_the_day[i].sub;
   card_1.append(h6_for_product);
 
   // append the card to the container outside the loop
   document.querySelector("div.common-row-col-deals").append(card_1);
 }
 
+// const category = document.querySelectorAll('.product-img')
+
+// category.forEach(function (add){
+//    add.addEventListener("click",(e)=> {
+//       const id = each.id;
+//       window.location.href = `./pages/products and detial pages/best of electronincs.html=${id}`;
+//    })
+// });
 
 // JSON FOR Furniture Bestseller
-const furniture_bestseller =[
-    {
-       "title":"Wardrobes",
-       "image":{
-            "source": "assets/images/products/z1.png",
-            "alt": "Wardrobes",
-         },
-       "price-offers":"₹4,777",
-       "sub":"2,3,4 doors",
-       "path":"./pages/products and detial pages/wardrobes.html",
+const furniture_bestseller = [
+  {
+    title: "Wardrobes",
+    image: {
+      source: "assets/images/products/z1.png",
+      alt: "Wardrobes",
     },
-        
-    {
-       "title":"Sofa",
-       "image":{
-            "source": "assets/images/products/z2.png",
-            "alt": "Sofa",
-         },
-       "price-offers":"₹7,777",
-       "sub":"Fabric & Leatherette",
-       "path":"",
-    },
+    "price-offers": "₹4,777",
+    sub: "2,3,4 doors",
+    path: "./pages/products and detial pages/wardrobes.html",
+  },
 
-    {
-       "title":"Office Study Table",
-       "image":{
-            "source": "assets/images/products/z5.png",
-            "alt": "Office Study Table",
-         },
-       "price-offers":"₹1,777",
-       "sub":"Work at Home like Boss",
-       "path":"",
+  {
+    title: "Sofa",
+    image: {
+      source: "assets/images/products/z2.png",
+      alt: "Sofa",
     },
+    "price-offers": "₹7,777",
+    sub: "Fabric & Leatherette",
+    path: "",
+  },
 
-    {
-       "title":"Best Selling Bed",
-       "image":{
-            "source": "assets/images/products/z5.png",
-            "alt": "Best Selling Bed",
-         },
-       "price-offers":"Upto 30% off",
-       "sub":"King, Queen & More",
-       "path":"",
+  {
+    title: "Office Study Table",
+    image: {
+      source: "assets/images/products/z5.png",
+      alt: "Office Study Table",
     },
+    "price-offers": "₹1,777",
+    sub: "Work at Home like Boss",
+    path: "",
+  },
 
-    {
-       "title":"Reclinear",
-       "image":{
-            "source": "assets/images/products/z7.png",
-            "alt": "Reclinear",
-         },
-       "price-offers":"₹11,777",
-       "sub":"Cozy Corners",
-       "path":"",
-    }
-]
+  {
+    title: "Best Selling Bed",
+    image: {
+      source: "assets/images/products/z5.png",
+      alt: "Best Selling Bed",
+    },
+    "price-offers": "Upto 30% off",
+    sub: "King, Queen & More",
+    path: "",
+  },
+
+  {
+    title: "Reclinear",
+    image: {
+      source: "assets/images/products/z7.png",
+      alt: "Reclinear",
+    },
+    "price-offers": "₹11,777",
+    sub: "Cozy Corners",
+    path: "",
+  },
+];
 
 // declare and initialize the variable outside the loop
 let card_2;
 
-for(let i = 0; i<=4; i++){
-
+for (let i = 0; i <= 4; i++) {
   // remove the re-declaration and re-initialization
   card_2 = document.createElement("div");
   card_2.setAttribute("class", "common-col");
 
-  a = document.createElement("a");
-  a.setAttribute("href", furniture_bestseller[i]["path"] );
+  const a = document.createElement("a");
+  a.setAttribute("href", furniture_bestseller[i].path);
   card_2.append(a);
 
-  img_of_electronics = document.createElement("img");
+  const img_of_electronics = document.createElement("img");
   img_of_electronics.setAttribute("class", "product-img");
-  img_of_electronics.setAttribute("src", furniture_bestseller[i]["image"]["source"]);
-  img_of_electronics.setAttribute("alt", furniture_bestseller[i]["image"]["alt"]);
+  img_of_electronics.setAttribute("src", furniture_bestseller[i].image.source);
+  img_of_electronics.setAttribute("alt", furniture_bestseller[i].image.alt);
   a.append(img_of_electronics);
 
-  h3_for_product = document.createElement("h3");
+  const h3_for_product = document.createElement("h3");
   h3_for_product.setAttribute("class", "product-title");
-  h3_for_product.innerText=(furniture_bestseller[i]["title"]);
+  h3_for_product.innerText = furniture_bestseller[i].title;
   card_2.append(h3_for_product);
 
-  h4_for_product = document.createElement("h4");
+  const h4_for_product = document.createElement("h4");
   h4_for_product.setAttribute("class", "price-title"); // add the missing prefix
-  h4_for_product.innerText=(furniture_bestseller[i]["price-offers"]);
+  h4_for_product.innerText = furniture_bestseller[i]["price-offers"];
   card_2.append(h4_for_product);
 
-  h6_for_product = document.createElement("h6");
+  const h6_for_product = document.createElement("h6");
   h6_for_product.setAttribute("class", "price");
-  h6_for_product.innerText=(furniture_bestseller[i]["sub"]);
+  h6_for_product.innerText = furniture_bestseller[i].sub;
   card_2.append(h6_for_product);
 
   // append the card to the container outside the loop
   document.querySelector("div.common-row-col-deals1").append(card_2);
 }
-
--- js/register.js
+++ js/register.js
@@ -1,54 +1,68 @@
-const Registered = e =>{
-    e.preventDefault();
-     let name = document.getElementById("name").value,
-      phonenumber = document.getElementById("phonenumber").value,
-      email = document.getElementById("email").value,
-      password = document.getElementById("password").value,
-      confirm_password = document.getElementById("confirm_password").value,
-      user_uniqueId = uuidv4();
-     // Parshing  the JSON
-     let user_list = JSON.parse(localStorage.getItem('user_list')) || [];
-     let exist = user_list.length &&
-     JSON.parse(localStorage.getItem('user_list')).some(data =>
-     data.email.toLowerCase()== email.toLowerCase() ||
-     data.phonenumber.toLowerCase() == phonenumber.toLowerCase());
-    
-     if (password != confirm_password){
-       return alert("password and confirm_password is not match")
-     }
-     if(!exist){
-        user_list.push({"name":name,"phonenumber":phonenumber,"email":email, password, confirm_password,user_uniqueId});
-        localStorage.setItem('user_list', JSON.stringify(user_list));
-        localStorage.setItem("user_data", JSON.stringify(email));
-        console.log(localStorage.getItem('user_list'));
-        document.querySelector('form').reset();
-        document.getElementById('email').focus();
-        alert('Account created Successfully');
-        location.href = "../../index.html";
-     }
-     else{
-        alert('Sorry the User already Exist!! \n Try with different Phone number or email');
-        document.querySelector('form').reset();
-    }  
-}
+const Registered = e => {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const phonenumber = document.getElementById("phonenumber").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirm_password = document.getElementById("confirm_password").value;
+  const user_uniqueId = crypto.randomUUID();
+  //   user_uniqueId = uuidv4();
+
+  // Parshing  the JSON
+  const user_list = JSON.parse(localStorage.getItem("user_list")) || [];
+  const exist =
+    user_list.length &&
+    JSON.parse(localStorage.getItem("user_list")).some(
+      data =>
+        data.email.toLowerCase() === email.toLowerCase() ||
+        data.phonenumber.toLowerCase() === phonenumber.toLowerCase()
+    );
+
+  if (password !== confirm_password) {
+    return alert("password and confirm_password is not match");
+  }
+  if (!exist) {
+    user_list.push({
+      name,
+      phonenumber,
+      email,
+      password,
+      confirm_password,
+      user_uniqueId,
+    });
+    localStorage.setItem("user_list", JSON.stringify(user_list));
+    localStorage.setItem("user_data", JSON.stringify(email));
+    console.log(localStorage.getItem("user_list"));
+    document.querySelector("form").reset();
+    document.getElementById("email").focus();
+    alert("Account created Successfully");
+    window.location.href = "../../index.html";
+  } else {
+    alert(
+      "Sorry the User already Exist!! \n Try with different Phone number or email"
+    );
+    document.querySelector("form").reset();
+  }
+};
 // for login()
-function login(e){
-    
-    let email = document.getElementById('email').value,
-        password = document.getElementById('password').value;    
-    let user_list = JSON.parse(localStorage.getItem('user_list')) || [];
-    let exist = user_list.length &&
-    JSON.parse(localStorage.getItem('user_list')).some(data =>
-         data.email.toLowerCase() == email.toLowerCase() && 
-         data.password.toLowerCase() == password.toLowerCase());
-         
-    if(exist){
-        alert("Your login is successful");
-        location.href = "../../index.html";
-        localStorage.setItem("user_data", JSON.stringify(email));
-    }
-    else{
-        alert("Incorrect login credentials");
-    }
-    e.preventDefault();
+function login(e) {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const user_list = JSON.parse(localStorage.getItem("user_list")) || [];
+  const exist =
+    user_list.length &&
+    JSON.parse(localStorage.getItem("user_list")).some(
+      data =>
+        data.email.toLowerCase() === email.toLowerCase() &&
+        data.password.toLowerCase() === password.toLowerCase()
+    );
+
+  if (exist) {
+    alert("Your login is successful");
+    window.location.href = "../../index.html";
+    localStorage.setItem("user_data", JSON.stringify(email));
+  } else {
+    alert("Incorrect login credentials");
+  }
+  e.preventDefault();
 }
